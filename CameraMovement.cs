17d8b22078bbe9c14e2f8ea42bba52cf71b6fcb2
AzeaOW <47344581+AzeaOW@users.noreply.github.com>
2019-12-13 16:06:17 -0700

    Add files via upload

---
+++ CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject parent;
+    private int targetLayer = 1 << 8; // Layer 8 (targets)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // When we have a horizontal value
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            // Move the attached parent based on the right vector of this object multiplied by the horizontal axis value
+            parent.transform.position += (transform.right) * Input.GetAxis("Horizontal");
+        }
+        
+
+        if (Input.anyKeyDown)
+        {
+            Debug.Log(Input.inputString);
+        }
+
+        
+    }
+}
---
+++ Jump.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class Jump : MonoBehaviour
+{
+    public bool isGrounded;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == ("Ground") && isGrounded == false)
+        {
+            isGrounded = true;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+}
